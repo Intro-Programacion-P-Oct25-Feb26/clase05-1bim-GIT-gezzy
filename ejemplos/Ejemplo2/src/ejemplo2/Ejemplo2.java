a7bebe558ce47641ecca6904fc71b0ca1cc7d875
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-11-12 12:20:28 -0500

    condicional compuesto

--- ejemplos/Ejemplo2/src/ejemplo2/Ejemplo2.java
+++ ejemplos/Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -28,17 +28,30 @@ public class Ejemplo2 {
         entrada.useLocale(Locale.US); // 10,2 / 10.2
         double promedio;
         String miNombre;
+        int edad;
+        String ciudad;
         System.out.println("Ingrese el nombre por favor");
         miNombre = entrada.nextLine();
         System.out.println("Ingrese el promedio por favor");
         promedio = entrada.nextDouble();
+        System.out.println("Ingrese la edad del estudiante");
+        edad = entrada.nextInt();
+         entrada.nextLine();
+        System.out.println("Ingrese la ciudad del estudiante");
+        ciudad = entrada.nextLine();
 
         if (promedio >= 7.5) {
-            System.out.printf("Estudiante %s aprobado con un promedio: %.2f\n", 
-                    miNombre, promedio);
+            System.out.printf("Estudiante: %s\n"
+                    + "Edad: %d\n"
+                    + "Ciudad: %s\n"
+                    + "Promedio: %.2f (Aprobado)", 
+                    miNombre, edad, ciudad, promedio);
         }else{
-            System.out.printf("Estudiante reprobado con un "
-                    + "promedio: %.2f\n", promedio);
+            System.out.printf("Estudiante: %s\n"
+                    + "Edad: %d\n"
+                    + "Ciudad: %s\n"
+                    + "Promedio: %.2f (Reprobado)", 
+                    miNombre, edad, ciudad, promedio);
         }
     }
     
